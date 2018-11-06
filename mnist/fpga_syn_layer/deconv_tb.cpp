619db49b041240d722ee851936113476c72f05d8
Jack <chl218@eng.ucsd.edu>
2018-11-05 21:04:21 -0800

    added tb

--- mnist/fpga_syn_layer/deconv_tb.cpp
+++ mnist/fpga_syn_layer/deconv_tb.cpp
@@ -3,12 +3,70 @@
 #include "deconv.h"
 
 // Mimicking the ARM Processor
-int main(void) {
+int main(int argc, char* arvg[]) {
+
+	printf("SHIFT_AMT:   %d\n", SHIFT_AMOUNT);
+	printf("TOTAL_BITS:  %d\n", TOTAL_BITS);
+	printf("UPPER_BOUND: %d\n", UPPER_BOUND);
+	printf("LOWER_BOUND: %d\n", LOWER_BOUND);
+
+	d_int in[10];
+	d_int out[28][28];
+
+	d_int kernel_1[4][4][32][10];
+	d_int kernel_2[6][6][32][32];
+	d_int kernel_3[6][6][32];
+
+	d_int bias_1[32];
+	d_int bias_2[32];
+	d_int bias_3;
+
+	d_int mean_1[32];
+	d_int mean_2[32];
+
+	d_int std_1[32];
+	d_int std_2[32];
 
 	int rand_size = 10;
 
+	FILE *fp = fopen("out.csv", "w");
 	for(int r = 0; r < rand_size; r++) {
+		set_random(in, r);
 
+		deconv(in,
+			   hidden1_weights,
+			   hidden2_weights,
+			   output_weights,
+			   hidden1_biases,
+			   hidden2_biases,
+			   output_biases[0],
+			   hidden1_mean,
+			   hidden2_mean,
+			   hidden1_std,
+			   hidden2_std,
+			   out);
+
+		save_image(out, fp);
 	}
+	fclose(fp);
 
 }
+
+// Initialize input layer
+void set_random(d_int in[10], int r) {
+	for(p_int k = 0; k < 10; k++) {
+		in[k] = random_1000[r][1][1][k];
+	}
+}
+
+// Write to file
+void save_image(d_int out, FILE *fp) {
+	for(int oh = 0; oh < 28; oh++) {
+		for(int ow = 0; ow < 28; ow++) {
+			if(oh + ow == 0) fprintf(fp,  "%d",  out[oh][ow]);
+			else 			 fprintf(fp, ",%d",  out[oh][ow]);
+		}
+	}
+	fprintf(fp, "\n");
+}
+
